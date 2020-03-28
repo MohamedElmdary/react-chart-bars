43aef14ca4679b2dee1c4f410bc5946d087c489e
MohamedElmdary <engm5081@gmail.com>
2020-03-28 23:43:53 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import BarChart from './components/bar-chart';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([
+        { value: 370, month: 'Jan' },
+        { value: 70, month: 'Feb' },
+        { value: 350, month: 'Oct' },
+        { value: 600, month: 'Nov' },
+        { value: 250, month: 'Jan3' },
+        { value: 50, month: 'Jan1' },
+        { value: 70, month: 'Feb2' },
+        { value: 350, month: 'Oct3' },
+        { value: 600, month: 'Nov4' },
+        { value: 325, month: 'Jan5' }
+    ]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            const d = [...data];
+            d[1].value += 150;
+            setData(d);
+        }, 1000);
+    }, [data]);
+
+    return (
+        <div>
+            <BarChart data={data} title="Total earning per month by L.E" />
+        </div>
+    );
 }
 
 export default App;
